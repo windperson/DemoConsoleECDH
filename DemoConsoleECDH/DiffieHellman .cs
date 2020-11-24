ec1fc50b775c491213afc692539c1dd5b3b4a1ac
Chen-Yu Pao <windperson@gmail.com>
2020-11-24 14:46:37 +0800

    Add project files.

---
+++ DemoConsoleECDH/DiffieHellman .cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DemoConsoleECDH
+{
+    public class DiffieHellman : IDisposable
+    {
+        private readonly Aes aes = null;
+        private readonly ECDiffieHellmanCng diffieHellman = null;
+
+        private readonly byte[] _publicKey;
+        public DiffieHellman()
+        {
+            this.aes = new AesCryptoServiceProvider();
+
+            this.diffieHellman = new ECDiffieHellmanCng
+            {
+                KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash,
+                HashAlgorithm = CngAlgorithm.Sha256
+            };
+
+            // This is the public key we will send to the other party
+            this._publicKey = this.diffieHellman.PublicKey.ToByteArray();
+        }
+        public byte[] PublicKey => _publicKey;
+
+        public byte[] IV => aes.IV;
+
+        public byte[] Encrypt(byte[] publicKey, string secretMessage)
+        {
+            byte[] encryptedMessage;
+            var key = CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob);
+            var derivedKey = this.diffieHellman.DeriveKeyMaterial(key); // "Common secret"
+
+            this.aes.Key = derivedKey;
+
+            using (var cipherText = new MemoryStream())
+            {
+                using (var encryptor = this.aes.CreateEncryptor())
+                {
+                    using (var cryptoStream = new CryptoStream(cipherText, encryptor, CryptoStreamMode.Write))
+                    {
+                        byte[] ciphertextMessage = Encoding.UTF8.GetBytes(secretMessage);
+                        cryptoStream.Write(ciphertextMessage, 0, ciphertextMessage.Length);
+                    }
+                }
+
+                encryptedMessage = cipherText.ToArray();
+            }
+
+            return encryptedMessage;
+        }
+        public string Decrypt(byte[] publicKey, byte[] encryptedMessage, byte[] iv)
+        {
+            string decryptedMessage;
+            var key = CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob);
+            var derivedKey = this.diffieHellman.DeriveKeyMaterial(key);
+
+            this.aes.Key = derivedKey;
+            this.aes.IV = iv;
+
+            using (var plainText = new MemoryStream())
+            {
+                using (var decryptor = this.aes.CreateDecryptor())
+                {
+                    using (var cryptoStream = new CryptoStream(plainText, decryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(encryptedMessage, 0, encryptedMessage.Length);
+                    }
+                }
+
+                decryptedMessage = Encoding.UTF8.GetString(plainText.ToArray());
+            }
+
+            return decryptedMessage;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                aes?.Dispose();
+
+                diffieHellman?.Dispose();
+            }
+        }
+    }
+}
---
+++ DemoConsoleECDH/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace DemoConsoleECDH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "ABCD";
+
+            using (var bob = new DiffieHellman())
+            {
+                using (var alice = new DiffieHellman())
+                {
+                    // Bob uses Alice's public key to encrypt his message.
+                    byte[] secretMessage = bob.Encrypt(alice.PublicKey, text);
+
+                    var transmitMsg = Convert.ToBase64String(secretMessage);
+                    var hexMsg = BitConverter.ToString(secretMessage).Replace("-", "");
+
+                    Console.WriteLine($"encrypted base64 = {transmitMsg}");
+                    Console.WriteLine($"encrypted hexStr = {hexMsg}");
+                    Console.ReadKey();
+
+                    // Alice uses Bob's public key and IV to decrypt the secret message.
+                    //string decryptedMessage = alice.Decrypt(bob.PublicKey, Convert.FromBase64String(transmitMsg), bob.IV);
+                    
+                    string decryptedMessage = alice.Decrypt(bob.PublicKey, StringToByteArray(hexMsg), bob.IV);
+
+                    Console.WriteLine($"\ndecrypted = {decryptedMessage}");
+                }
+            }
+            Console.ReadKey();
+
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
